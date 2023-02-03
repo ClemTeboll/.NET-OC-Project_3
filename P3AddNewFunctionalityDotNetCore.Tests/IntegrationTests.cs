d965b03fd66f08dd2b6bbf536812d03620034e53
Clément <clement.llobet@jasparo.fr>
2023-02-03 15:03:50 +0100

    cretate integration test file

---
+++ P3AddNewFunctionalityDotNetCore.Tests/IntegrationTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.IntegrationTests
+{
+    public class IntegrationTests
+    {
+
+        [Fact]
+        public void CheckProductIsSaved()
+        {
+            // Arrange
+            Mock<ICart> mockCart = new Mock<ICart>();
+            Mock<IOrderRepository> mockOrder = new Mock<IOrderRepository>();
+            Mock<IProductRepository> mockProductRepository = new Mock<IProductRepository>();
+            Mock<IProductService> mockProductService = new Mock<IProductService>();
+            Mock<IStringLocalizer<ProductService>> mockStringLocalizer = new Mock<IStringLocalizer<ProductService>>();
+
+            var productService = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrder.Object, mockStringLocalizer.Object);
+
+            ProductViewModel product = new ProductViewModel()
+            {
+                Name = "Product",
+                Price = "11",
+                Description = "Test",
+                Stock = "0",
+                Details = "test"
+            };
+
+            // Act
+            productService.SaveProduct(product);
+
+            //bool expected = true;
+            //bool actual;
+
+            var options = new DbContextOptionsBuilder<AppIdentityDbContext>()
+                .UseInMemoryDatabase(databaseName: "Identity")
+                .Options;
+
+            using (var context = new AppIdentityDbContext(options))
+            {
+                // actual
+            }
+
+            // Assert
+
+            //Assert.True(expected == actual);
+        }
+
+        //[Fact]
+        //public void CheckProductIsDeleted()
+        //{
+        //    // Arrange
+
+        //    // Act
+
+        //    // Assert
+        //}
+    }
+}
